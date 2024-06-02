81f0a53e800f643f868b3e805465841009f2da07
Petr Škoda <skodapetr@gmail.com>
2024-06-02 15:30:34 +0200

    Update navigation dropdown
    #18

--- packages/core/src/components/gov-nav/gov-nav-dropdown.tsx
+++ packages/core/src/components/gov-nav/gov-nav-dropdown.tsx
@@ -1,16 +1,24 @@
 import { Component, Element, h, Host } from '@stencil/core'
 
+enum State {
+  Close,
+  Open,
+  Opening,
+}
+
 @Component({
-	tag: 'gov-nav-dropdown',
-	styleUrl: 'gov-nav-dropdown.scss',
+  tag: 'gov-nav-dropdown',
+  styleUrl: 'gov-nav-dropdown.scss',
 })
 export class GovNavItem {
-	@Element() readonly host: HTMLGovNavDropdownElement
+  @Element() readonly host: HTMLGovNavDropdownElement
 
   /**
    * Store listener used to listen for global click event.
    */
   private documentOnClickListener
+
+  private state = State.Close
 
   componentDidLoad() {
     this.documentOnClickListener = () => this.onClose()
@@ -22,24 +30,48 @@ export class GovNavItem {
   }
 
   private onClose() {
-    this.host.classList.remove("open")
+    switch (this.state) {
+      case State.Close:
+        // Nothing to do here.
+        break
+      case State.Open:
+        // Close.
+        this.host.classList.remove("open")
+        this.state = State.Close
+        break
+      case State.Opening:
+        // Has been open in this event as the click bubbles to document.
+        this.state = State.Open
+        break;
+    }
   }
 
-  private onToggle(event) {
-    event.stopPropagation();
-    this.host.classList.toggle("open")
+  private onToggle() {
+    switch (this.state) {
+      case State.Close:
+        this.host.classList.add("open",)
+        this.state = State.Opening
+        break
+      case State.Open:
+        this.host.classList.remove("open")
+        this.state = State.Close
+        break;
+      case State.Opening:
+        // This should not happen.
+        break
+    }
   }
 
-	render() {
-		return (
-			<Host class={"gov-nav-dropdown"} onClick={(event) => this.onToggle(event)}>
+  render() {
+    return (
+      <Host class={"gov-nav-dropdown"} onClick={() => this.onToggle()}>
         <div class="gov-nav-item__link">
           <slot name="label" />
         </div>
         <div class="dropdown">
           <slot name="dropdown" />
         </div>
-			</Host>
-		)
-	}
+      </Host>
+    )
+  }
 }
